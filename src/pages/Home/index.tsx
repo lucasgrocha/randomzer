c56701aef8880b70b4e1f95bc6f34c542a8ee3c0
Lucas Rocha <lucasgeorgerocha@gmail.com>
2021-01-30 11:25:43 -0300

    styles & jsx -> Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
-import '../../assets/styles/global.css'
+import React, { useState } from 'react';
+import '../../assets/styles/global.css';
+import './styles.css';
 
 const Home: React.FC = () => {
+  const [words, setWords] = useState<string[]>([]);
+  const [word, setWord] = useState<string>('');
+
+  function handleKeyPress(evt: React.KeyboardEvent) {
+    if (evt.key === 'Enter' && word.length) {
+      appendWord()
+    }
+  }
+
+  function appendWord() {
+    setWords((prev) => [...prev, word]);
+    setWord('');
+  }
+
   return (
-    <div>
-      <h1></h1>
+    <div className="container">
+      <div id="input-field">
+        <input
+          type="text"
+          id="word-input"
+          value={word}
+          onChange={(e) => setWord(e.target.value)}
+          onKeyPress={handleKeyPress}
+        />
+        <button onClick={appendWord}>+</button>
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
