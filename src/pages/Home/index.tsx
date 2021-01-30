6e4b953201e66aebe0cda3d6f8a3815012bcf727
Lucas Rocha <lucasgeorgerocha@gmail.com>
2021-01-30 10:37:46 -0300

    base structure

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../assets/styles/global.css'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+}
+
+export default Home;
